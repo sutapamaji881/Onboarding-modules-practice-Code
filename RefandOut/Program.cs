4f0ca35d9fb36b18e8fd4210ffd3bebf61894e69
Sutapa_Maji <Sutapa_Maji@EPINHYDW0894>
2021-04-20 19:10:25 +0530

    RefandOutPractice code

---
+++ RefandOut/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RefandOut
+{
+    class Program
+    {
+        public static void Sum(out int G)
+        {
+            G = 80;
+            G += G;
+        }
+
+        static void SetValue(ref string str1)
+        {
+            if (str1 == "EPAM")
+            {
+                Console.WriteLine("Hello!!world");
+            }
+            str1 = "EPAMSystems";
+        }
+        static void Main(string[] args)
+        {
+            int G;
+            string str = "EPAM";
+            Sum(out G);
+            Console.WriteLine("The sum of" +" the value is: {0}", G);
+            SetValue(ref str);
+            Console.WriteLine(str);
+        }
+    }
+}
